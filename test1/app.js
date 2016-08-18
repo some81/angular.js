a9ac815e4adbddd15f8bf46ec910d5a4eed08194
root <root@ip-10-45-49-236.ec2.internal>
2016-08-18 16:56:39 -0400

    update

--- test1/app.js
+++ test1/app.js
@@ -1,27 +1,60 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(function($routeProvider){
-   
+myApp.config(function ($routeProvider) {
+    
     $routeProvider
-    .when('/',{
+    
+    .when('/', {
         templateUrl: 'pages/main.htm',
         controller: 'mainController'
     })
-    .when('/second',{
+    
+    .when('/second', {
         templateUrl: 'pages/second.htm',
-        controller: 'scondController'
-    });
+        controller: 'secondController'
+    })
+    
+    .when('/second/:num', {
+        templateUrl: 'pages/second.htm',
+        controller: 'secondController'
+    })
     
 });
 
-myApp.controller('mainController', ['$scope', '$log', function($scope, $log){
+myApp.service('nameService', function() {
+   
+    var self = this;
+    this.name = 'John Doe';
     
+    this.namelength = function() {
+      
+        return self.name.length;
+        
+    };
     
+});
+
+myApp.controller('mainController', ['$scope', '$log', 'nameService', function($scope, $log, nameService) {
+    
+    $scope.name = nameService.name;
+    
+    $scope.$watch('name', function() {
+        nameService.name = $scope.name;
+    });
+    
+    $log.log(nameService.name);
+    $log.log(nameService.namelength());
     
 }]);
 
-myApp.controller('secondController', ['$scope', '$log', function($scope, $log){
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', 'nameService', function($scope, $log, $routeParams, nameService) {
     
+    $scope.num = $routeParams.num || 1;
     
+    $scope.name = nameService.name;
+    
+    $scope.$watch('name', function() {
+        nameService.name = $scope.name;
+    });
     
 }]);
